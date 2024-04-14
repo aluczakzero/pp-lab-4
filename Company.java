ae451ebaf72223997ec59cbe0f86df8b7017ebe6
Adrian <adrianl2710@wp.pl>
2024-04-14 11:28:58 +0200

    Zadanie 2

--- Company.java
+++ Company.java
@@ -1,12 +1,14 @@
+import employees.*;
+
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Employee("Jan Nowak", 10000);
-        employees[1] = new Employee("Roman Kowalski", 30000);
+        employees[0] = new Manager("Jan Nowak", 10000);
+        employees[1] = new Worker("Roman Kowalski", 30000);
         employees[2] = new Employee("Michał Wiśniewski", 25000);
         employees[3] = new Employee("Kamil Kowalski", 40000);
-        employees[4] = new Employee("Łukasz Nowak", 60000);
+        employees[4] = new Worker("Łukasz Nowak", 60000);
 
         
         System.out.println("Dane pracownika o indeksie 3: " + employees[3]);
